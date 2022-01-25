05e94025cb81f3df5d783594c86f79627bc2ada7
Anthony <ajsur1017@gmail.com>
2022-01-25 11:16:52 -0500

    completed practice app

---
+++ app.js
@@ -0,0 +1,149 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        loggedin: false,
+        JWT: "",
+        createUN: "",
+        createPW: "",
+        loginUN: "",
+        loginPW: "",
+        devURL: "http://localhost:3000",
+        prodURL: null,
+        user: null,
+        token: null,
+        notes: [],
+        newNote: "",
+        updateNote: "",
+        editID: 0
+    },
+    methods: {
+        handleLogin: function () {
+            const URL = this.prodURL ? this.prodURL : this.devURL
+            const user = { username: this.loginUN, password: this.loginPW }
+            fetch(`${URL}/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        alert('error logging in')
+                    } else {
+                        this.user = data.user
+                        this.token = data.token
+                        this.loggedin = true
+                        this.getNotes()
+                        this.loginPW = ""
+                        this.loginUN = ""
+                        window.sessionStorage.setItem('login', JSON.stringify(data))
+                    }
+
+                });
+        },
+        handleLogout: function () {
+            this.loggedin = false
+            this.user = null
+            this.token = null
+            window.sessionStorage.removeItem('login')
+        },
+        handleSignup: function () {
+            const URL = this.prodURL ? this.prodURL : this.devURL
+            const user = { username: this.createUN, password: this.createPW }
+            fetch(`${URL}/users`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: user
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        console.log(`Sign Up Unsucessful: Error: ${data.error}`)
+                        alert(`Sign Up Unsucessful: Error: ${data.error}`)
+                    } else {
+                        console.log('Sign Up Successful')
+                        alert('Sign Up Successful')
+                    }
+                })
+        },
+        getNotes: function () {
+            const URL = this.prodURL ? this.prodURL : this.devURL
+            fetch(`${URL}/notes`, {
+                method: "GET",
+                headers: {
+                    Authorization: `bearer ${this.token}`
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    this.notes = data
+                })
+        },
+        createNote: function () {
+            const URL = this.prodURL ? this.prodURL : this.devURL
+            const note = { message: this.newNote }
+            fetch(`${URL}/notes`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `bearer ${this.token}`
+                },
+                body: JSON.stringify(note)
+            })
+                .then(response => {
+                    this.newNote = ""
+                    this.getNotes()
+                })
+
+        },
+        deleteNote: function (event) {
+            const URL = this.prodURL ? this.prodURL : this.devURL
+            const ID = event.target.id
+            fetch(`${URL}/notes/${ID}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: `bearer ${this.token}`
+                }
+            })
+                .then(response => {
+                    this.getNotes()
+                })
+        },
+        editNote: function (event) {
+            const URL = this.prodURL ? this.prodURL : this.devURL;
+            const ID = event.target.id;
+            const updated = { message: this.updateNote }
+            fetch(`${URL}/notes/${ID}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `bearer ${this.token}`
+                },
+                body: JSON.stringify(updated)
+            })
+                .then((response) => {
+                    this.getNotes();
+                });
+        },
+        editSelect: function (event) {
+            this.editID = event.target.id
+            console.log(this.editID = event.target.id)
+
+            const theNote = this.notes.find((note) => {
+                return note.id == this.editID
+            })
+            this.updateNote = theNote.message
+        },
+    },
+    created: function(){
+        const getLogin = JSON.parse(window.sessionStorage.getItem('login'))
+        this.user = getLogin.user;
+        this.token = getLogin.token;
+        this.loggedin = true;
+        this.getNotes();
+    }
+})
